8426047852c3ec58aa61017a1423828a18f5b450
Lama-Albayomi <43874780+Lama-Albayomi@users.noreply.github.com>
2018-10-05 11:29:29 +0300

    Watermelons are verywhere

---
+++ lab#2 version control/Assets/MelonEvrywhere.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MelonEvrywhere : MonoBehaviour {
+
+    public GameObject Karpuz;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(Karpuz,new Vector2 (Random.Range(-5f,5f),Random.Range(-10f,10f)), Quaternion.identity);
+        }
+	}
+}
